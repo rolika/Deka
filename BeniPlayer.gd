f205dedebfa25a6b6ee2df931fc1c8d972b5747a
Weisz Roland <weisz.roland@pohlen-dach.hu>
2023-01-06 17:56:50 +0100

    player moves left and right

--- BeniPlayer.gd
+++ BeniPlayer.gd
@@ -1,10 +1,26 @@
 extends Area2D
 
 
+var speed = 400
+var screen_size
+
+
 func _ready():
 	self.position.x = 460
 	self.position.y = 400
+	screen_size = get_viewport_rect().size
 
-func _process(_delta):	
+func _process(delta):	
+	var velocity = Vector2.ZERO  # player's movement vector
 	if Input.is_action_pressed("ui_head"):
 		 $AnimationPlayer.play("jump")
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	
+	velocity = velocity.normalized() * speed
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
